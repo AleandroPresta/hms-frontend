3d51d121e645054bd530e79a99631a95596dee39
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-31 11:29:29 +0200

    Changed request handling from subscribe to stream.

--- src/app/rooms/room-page/room-page.component.ts
+++ src/app/rooms/room-page/room-page.component.ts
@@ -1,40 +1,31 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { RoomsService } from '../rooms.service';
+import { map, Observable, switchMap } from 'rxjs';
 import { Room } from '../Room';
+import { RoomsService } from '../rooms.service';
 
 @Component({
   selector: 'app-room-page',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgIf],
   templateUrl: './room-page.component.html',
-  styleUrl: './room-page.component.css'
+  styleUrl: './room-page.component.css',
 })
-export class RoomPageComponent implements OnInit {
-  id: number = -1;
-  room: Room = {
-    id: -1,
-    type: '',
-    price: 0,
-    rating: 0,
-    available: false,
-  }
+export class RoomPageComponent {
 
-  constructor(
-    private route: ActivatedRoute,
-    private roomsService: RoomsService
-  ) { }
+  id$: Observable<number>;
+  room$: Observable<Room>;
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.id = params['id'];
-    });
 
-    this.roomsService.getRoom(this.id).subscribe(
-      (room: Room) => {
-        this.room = room;
-        console.log('room.available', room.available);
-      }
+  constructor(private router: ActivatedRoute, private roomService: RoomsService) {
+    this.id$ = this.router.params.pipe(
+      map(params => params['id'])
+    );
+
+    this.room$ = this.id$.pipe(
+      switchMap(id => this.roomService.getRoom(id))
     );
   }
+
 }
