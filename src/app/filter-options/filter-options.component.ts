d6d7b51823cc2632218db009ddd463fe2f47e28a
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-10 13:14:44 +0200

    Added checkbox for filtering by type (tofix: pressing the label of the box activates the change but doesn't check the box)

--- src/app/filter-options/filter-options.component.ts
+++ src/app/filter-options/filter-options.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FilterOptions } from './options';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-filter-options',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './filter-options.component.html',
   styleUrl: './filter-options.component.css'
 })
 export class FilterOptionsComponent {
+
+  options: FilterOptions[] = [
+    { name: 'Single', selected: false },
+    { name: 'Double', selected: false },
+    { name: 'King', selected: false },
+    { name: 'Queen', selected: false },
+    { name: 'Suite', selected: false },
+  ]
+
+  filterByType() {
+    console.log('Filtering by type');
+  }
 
 }
---
+++ src/app/filter-options/options.ts
@@ -0,0 +1,4 @@
+export interface FilterOptions {
+    name: string;
+    selected: boolean;
+}
