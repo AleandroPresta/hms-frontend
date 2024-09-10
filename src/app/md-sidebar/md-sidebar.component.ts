8eaa6e804b61c30498d498b1ce19e58f0ce80ece
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-10 12:19:33 +0200

    Added filter options

--- src/app/md-sidebar/md-sidebar.component.ts
+++ src/app/md-sidebar/md-sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FilterOptionsComponent } from '../filter-options/filter-options.component';
 
 @Component({
   selector: 'app-md-sidebar',
   standalone: true,
   imports: [
-
+    FilterOptionsComponent
   ],
   templateUrl: './md-sidebar.component.html',
   styleUrl: './md-sidebar.component.css'
