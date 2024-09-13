34a1035f6f8535a190cd442c5ae047394aaa142d
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-13 11:48:29 +0200

    Changed checkbox system to radio system for filtering by price range

--- src/app/filter-options/filter-options.component.ts
+++ src/app/filter-options/filter-options.component.ts
@@ -23,9 +23,16 @@ export class FilterOptionsComponent {
     console.log(this.roomTypeOptions)
   }
 
+  // Only one price range option can be selected at a time
   togglePriceRange(i: number) {
     // Toggle the option selected state
     this.priceRangeOptions[i].selected = !this.priceRangeOptions[i].selected
+    // Unselect all other options
+    this.priceRangeOptions.forEach((option, index) => {
+      if (index !== i) {
+        option.selected = false
+      }
+    })
     console.log(this.priceRangeOptions)
   }
 
