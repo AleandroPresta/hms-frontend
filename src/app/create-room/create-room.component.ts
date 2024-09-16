334e19d6c3550d522b3fe94eca6551a5cb746aa3
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-16 11:37:49 +0200

    Created the select for the room type

--- src/app/create-room/create-room.component.ts
+++ src/app/create-room/create-room.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Room } from '../rooms/Room';
 
 @Component({
   selector: 'app-create-room',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-room.component.html',
   styleUrl: './create-room.component.css'
 })
 export class CreateRoomComponent {
 
+  room: Room = {
+    type: '',
+    price: 0,
+    rating: 0,
+    available: true
+  }
+
+  submitForm() {
+    console.log('Form submitted');
+    console.log(this.room);
+  }
 }
