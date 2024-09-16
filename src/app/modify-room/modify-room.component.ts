584807da7b030388529d79898a81bf46cae03df5
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-16 16:14:05 +0200

    Created modify-room component

---
+++ src/app/modify-room/modify-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-room.component.html',
+  styleUrl: './modify-room.component.css'
+})
+export class ModifyRoomComponent {
+
+}
