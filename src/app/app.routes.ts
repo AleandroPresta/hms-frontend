d399289fb465d29acd57c76fbd3cc38083838397
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-16 11:13:24 +0200

    Created create-room component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomPageComponent } from './rooms/room-page/room-page.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'room', component: RoomsComponent },
     { path: 'room/:id', component: RoomPageComponent },
+    { path: 'add-room', component: CreateRoomComponent },
     { path: '**', redirectTo: 'home' }
 ];
