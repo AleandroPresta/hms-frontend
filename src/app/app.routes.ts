b9218a73b66cc35a1dfae7d55caf74bdd23af175
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-16 16:14:56 +0200

    Created modify-room routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomPageComponent } from './rooms/room-page/room-page.component';
 import { CreateRoomComponent } from './create-room/create-room.component';
+import { ModifyRoomComponent } from './modify-room/modify-room.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'room', component: RoomsComponent },
     { path: 'room/:id', component: RoomPageComponent },
+    { path: 'room/:id/modify-room', component: ModifyRoomComponent },
     { path: 'add-room', component: CreateRoomComponent },
     { path: '**', redirectTo: 'home' }
 ];
