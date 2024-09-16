59093856d4d0d0d1743a9d4bd68291f7e432d803
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-09-16 17:23:51 +0200

    Added modify-room routing also to the visit room page

--- src/app/rooms/room-page/room-page.component.ts
+++ src/app/rooms/room-page/room-page.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { map, Observable, switchMap } from 'rxjs';
 import { Room } from '../Room';
 import { RoomsService } from '../rooms.service';
@@ -8,7 +8,7 @@ import { RoomsService } from '../rooms.service';
 @Component({
   selector: 'app-room-page',
   standalone: true,
-  imports: [AsyncPipe, NgIf],
+  imports: [AsyncPipe, NgIf, RouterLink],
   templateUrl: './room-page.component.html',
   styleUrl: './room-page.component.css',
 })
